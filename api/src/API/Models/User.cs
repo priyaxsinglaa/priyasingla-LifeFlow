120a913e0a3c2083e8415e73ab134a4bba5ccb25
priya singla <asusual.priya@gmail.com>
2026-05-11 16:43:31 +0530

    authentication created

---
+++ api/src/API/Models/User.cs
@@ -0,0 +1,22 @@
+﻿namespace API.Models;
+
+public partial class User
+{
+    public int Id { get; set; }
+
+    public string FullName { get; set; } = null!;
+
+    public string Email { get; set; } = null!;
+
+    public string PhoneNumber { get; set; } = null!;
+
+    public string PasswordHash { get; set; } = null!;
+
+    public string? BloodGroup { get; set; }
+
+    public string? Disease { get; set; }
+
+    public DateTime? LastDonationDate { get; set; }
+
+    public DateTime? CreatedAt { get; set; }
+}
